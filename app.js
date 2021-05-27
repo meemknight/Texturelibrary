273e7f59d30d5907ffca9bdce773656234f0a3a6
vlod <vladalex420@gmail.com>
2021-05-27 22:50:21 +0300

    added video

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.get("/page/:id", (req, res) =>{
 
 })
 
+//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
 app.get("/galery", (req, res) =>
 {
     const textFisier=fs.readFileSync(path.join(__dirname, "/public/resources/img.json"));
@@ -41,8 +51,14 @@ app.get("/galery", (req, res) =>
         imgs[i.sfert_ora-1].push(rootPath + i.cale_imagine);
     });
 
-    
-    res.render("pagini/galery", {galery: imgs[0]}) 
+    let d = new Date();
+    m = d.getMinutes();
+    m = Math.floor(m /= 15);
+
+    shuffle(imgs[m]);
+    imgs[m] = imgs[m].slice(0, 10);
+
+    res.render("pagini/galery", {galery: imgs[m]}) 
 
     console.log("galery render")
 })
@@ -50,7 +66,30 @@ app.get("/galery", (req, res) =>
 app.get(["/", "index.html", "index"], (req, res) => {
     //req cererea
     //res respons
-    res.render("pagini/index") 
+
+    galery = [
+        'geam25_05_202113_03_59.png',
+        'geam25_05_202113_04_14.png',
+        'geam25_05_202113_04_21.png', 
+        'geam25_05_202113_07_13.png', 
+        'geam25_05_202113_07_25.png', 
+        'geam25_05_202113_08_33.png', 
+        'geam25_05_20213_02_07.png', 
+        'geam25_05_20213_02_40.png', 
+        'im1.png', 
+        'im2.png', 
+        'im3.png', 
+        'im4.png', 
+        'im5.png'];
+
+    p = "/resources/imgs/"
+
+    for(let i =0; i<galery.length; i++)
+    {
+        galery[i] = p + galery[i]
+    }
+
+    res.render("pagini/index", {liveGalery: galery[0]})
 
     console.log("index render")
 })
